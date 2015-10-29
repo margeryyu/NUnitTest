6c617f097e77cfb840f58aba3c874fa85b78f9ae
margeryyu <margeryyu@sina.com>
2015-10-29 17:07:29 +0800

    create nuint project

---
+++ Calculator/Calculator.Test/Class1.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+
+namespace Calculator.Test
+{
+   [TestFixture]
+   public class CalculatorTests
+   {
+      [Test]
+      public void ShouldAddTwoNumbers()
+      {
+         var sut = new SimpleCalculator();
+         var result = sut.Add(3.5m, 2m);
+         Assert.That(result, Is.EqualTo(5.5m));
+      }
+
+      [Test]
+      public void ShouldMultipleTwoNumbers()
+      {
+         var sut = new SimpleCalculator();
+         var result = sut.multiply(3.5m, 2m);
+         Assert.That(result, Is.EqualTo(7m));
+      }
+   }
+}
---
+++ Calculator/Calculator/Calculator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class SimpleCalculator
+    {
+       public decimal Add(decimal a, decimal b)
+       {
+          return a + b;
+       }
+
+       public decimal multiply(decimal a, decimal b)
+       {
+          // bug for demo purposes
+          return a + b;
+       }
+    }
+}
