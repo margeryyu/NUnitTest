5f1189ecc6cba864da9c276fb11c20f7acac6ab5
margeryyu <margeryyu@sina.com>
2015-10-30 11:24:18 +0800

    Add JoinString

--- Calculator/Calculator.Test/SimpleCalculatorTests.cs
+++ Calculator/Calculator.Test/SimpleCalculatorTests.cs
@@ -1,4 +1,5 @@
 ﻿using NUnit.Framework;
+using JoinString;
 
 namespace Calculator.Test
 {
@@ -19,6 +20,30 @@ namespace Calculator.Test
          var sut = new SimpleCalculator();
          var result = sut.multiply(3.5m, 2m);
          Assert.That(result, Is.EqualTo(7m));
+      }
+
+      [Test]
+      public void JoinString_Equality()
+      {
+         var js = new JoinTwoString();
+         var result = js.Join("Margery", "Yu");
+         Assert.That(result, Is.EqualTo("Margery Yu"));
+      }
+
+      [Test]
+      public void JoinString_Equality_IgnoreCaseSensitive()
+      {
+         var js = new JoinTwoString();
+         var result = js.Join("Margery", "Yu");
+         Assert.That(result, Is.EqualTo("MARGERY YU").IgnoreCase);
+      }
+
+      [Test]
+      public void JoinString_NotEquality()
+      {
+         var js = new JoinTwoString();
+         var result = js.Join("Margery", "Yu");
+         Assert.That(result, Is.Not.EqualTo("MARGERY WANG").IgnoreCase);
       }
    }
 }
