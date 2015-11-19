6c7ae585298601e822940f63c48f6c5bb753baba
margeryyu <margeryyu@sina.com>
2015-11-19 16:22:16 +0800

    Add new examples

--- Calculator/Calculator.Test/NunitTestsExamples.cs
+++ Calculator/Calculator.Test/NunitTestsExamples.cs
@@ -116,27 +116,6 @@ namespace Calculator.Test
    }
 
 
-   //TestFixture 2 : Simple NUnit test examples
-   [TestFixture]
-   public class MemoryCalculatorTests
-   {
-      MemoryCalculator sut = new MemoryCalculator();
-
-      [Test]
-      public void ShouldAdd()
-      {
-         sut.Add(10);
-         sut.Add(5);
-         Assert.That(sut.CurrentValue, Is.EqualTo(15));
-      }
-
-      [Test]
-      public void ShouldSubtract()
-      {
-         sut.Subtract(5);
-         Assert.That(sut.CurrentValue, Is.EqualTo(-5));
-      }
-
-
-   }
+   
+   
 }
